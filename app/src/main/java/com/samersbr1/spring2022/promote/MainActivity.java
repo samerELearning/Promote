19bdf73def6f4f1fa9574735815cfcdd663ac103
samerelearning <samerelearning@outlook.com>
2022-05-03 00:14:43 +0300

    Implemented Sign Up page

--- app/src/main/java/com/samersbr1/spring2022/promote/MainActivity.java
+++ app/src/main/java/com/samersbr1/spring2022/promote/MainActivity.java
@@ -2,6 +2,7 @@ package com.samersbr1.spring2022.promote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     public void signUp(View view)
     {
      //this method will switch activity to the sign up page
-
+        Intent intent = new Intent(getApplicationContext(), SignUpPage.class);
+        startActivity(intent);
     }
 
     public void signInAccess(View view)
---
+++ app/src/main/java/com/samersbr1/spring2022/promote/SignUpPage.java
@@ -0,0 +1,30 @@
+package com.samersbr1.spring2022.promote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
+
+    public void signIn(View view)
+    {
+     //this method will switch activity to the sign in page
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void influencerSignUp(View view)
+    {
+     //this method will switch activity to the influencer sign up page
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);//Change to influencer sign up page
+        startActivity(intent);
+    }
+}
