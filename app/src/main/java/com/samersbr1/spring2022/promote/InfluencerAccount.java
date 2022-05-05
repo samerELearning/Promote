867b69f4eb2a43e613902ba951a476dd65912e60
samerelearning <samerelearning@outlook.com>
2022-05-05 19:40:39 +0300

    Implemented influencer front-end user profile

--- app/src/main/java/com/samersbr1/spring2022/promote/InfluencerAccount.java
+++ app/src/main/java/com/samersbr1/spring2022/promote/InfluencerAccount.java
@@ -2,7 +2,9 @@ package com.samersbr1.spring2022.promote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class InfluencerAccount extends AppCompatActivity {
 
@@ -13,4 +15,40 @@ public class InfluencerAccount extends AppCompatActivity {
     }
 
 
+    public void profilePicture(View view)
+    {
+     //This method will open the account information
+
+    }
+
+    public void logOut(View view)
+    {
+
+    }
+
+    public void instagram(View view)
+    {
+
+    }
+
+    public void feed(View view)
+    {
+        //this method will switch activity to the feed page
+        Intent intent = new Intent(getApplicationContext(), InfluencerFeed.class);
+        startActivity(intent);
+    }
+
+    public void likes(View view)
+    {
+        //this method will switch activity to the likes page
+        Intent intent = new Intent(getApplicationContext(), InfluencerLikes.class);
+        startActivity(intent);
+    }
+
+    public void matches(View view)
+    {
+        //this method will switch activity to the matches page
+        Intent intent = new Intent(getApplicationContext(), InfluencerMatches.class);
+        startActivity(intent);
+    }
 }
